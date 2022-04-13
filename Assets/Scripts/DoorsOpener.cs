ea1a3a548daee5336cdd0f9ef1935bbbbe10ff45
Asylbek Karashev <asylbekka@asisnur.kg>
2022-04-13 17:39:07 +0600

    building timeline, flames with sounds, doors open animation track

---
+++ Assets/Scripts/DoorsOpener.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorsOpener : MonoBehaviour
+{
+    private HingeJoint [] doorJoints = null;
+    private void Start()
+    {
+        doorJoints = GetComponentsInChildren<HingeJoint>();
+    }
+
+    public void OpenDoors()
+    {
+        if(doorJoints != null)
+        {
+            foreach (HingeJoint item in doorJoints)
+            {
+                item.useMotor = true;
+            }
+        }
+    }
+}
